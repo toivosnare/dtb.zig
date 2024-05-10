461f13c9248090c155f9ebea94a736299eb9d932
Toivo Snåre <topisn@gmail.com>
2024-05-10 18:39:16 +0300

    zig 0.12

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("dtb", .{
-        .source_file = .{ .path = "src/dtb.zig" },
+        .root_source_file = .{ .path = "src/dtb.zig" },
     });
 
     const test_step = b.step("test", "Run the tests");
